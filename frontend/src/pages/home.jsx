693448f22f4babcc4f60bdb7a89fe4b103493f79
Meet Raut <meetraut3004@gmail.com>
2024-03-29 23:40:58 +0530

    update home

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import '../components/css/home.css'
 
 function Home(){
   return (
-    <div><h1>Home</h1></div>
+    <div>
+      <div class="title">
+        <h1>ROBOZON</h1>
+      </div>
+      <div class="textBox">
+        <p>Building the Future & Empowering Minds Through Robotics!</p>
+      </div>
+      <div class="button">
+        <a href="product.html" class="btn">Explore NOW!</a>
+      </div>
+    </div>
   )
 }
 
