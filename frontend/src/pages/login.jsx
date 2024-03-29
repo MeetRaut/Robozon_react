56fc9b8b61ddd673ab76f06056f187557a88f4fc
Meet Raut <meetraut3004@gmail.com>
2024-03-30 00:52:23 +0530

    update login

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import './css/login.css'
 
 function Login(){
   return (
-    <div><h1>LOGIN</h1></div>
+    <div>
+      
+      <div class="login-input">
+              <h1>LOGIN</h1>
+              <div class ="login-input-box">
+                  <input type ="text" placeholder="Username" required/>
+              </div>
+              <div class="login-input-box">
+                  <input type ="password" placeholder="Enter your Password" required/>
+              </div>
+              
+            <a href="/"><button type ="Submit" class="btn">Login</button></a>
+              <div class ="register-link">
+                <p>Don't have an account?</p>
+                <a href ="#">Sign Up</a>
+              </div>
+        </div>
+    </div>
   )
 }
 
